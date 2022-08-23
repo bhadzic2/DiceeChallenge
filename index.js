01236feaf967ac0b60cf35c3198c298b31dc9115
Belma Hadžić <bhadzic2@etf.unsa.ba>
2022-08-23 16:21:17 +0200

    added random dices

---
+++ index.js
@@ -0,0 +1,2 @@
+document.querySelector(".img1").setAttribute("src","images/dice"+Math.floor(1+Math.random()*6)+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+Math.floor(1+Math.random()*6)+".png");
