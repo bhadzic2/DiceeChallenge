b3e7ac9211d7ae36e5167eff2a37358f0c766a4b
Belma Hadžić <bhadzic2@etf.unsa.ba>
2022-08-23 16:30:36 +0200

    added winner

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-document.querySelector(".img1").setAttribute("src","images/dice"+Math.floor(1+Math.random()*6)+".png");
-document.querySelector(".img2").setAttribute("src","images/dice"+Math.floor(1+Math.random()*6)+".png");
+
+var num1=Math.floor(1+Math.random()*6);
+var num2=Math.floor(1+Math.random()*6);
+document.querySelector(".img1").setAttribute("src","images/dice"+num1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+num2+".png");
+if(num1>num2){
+  document.querySelector("h1").innerHTML="Player 1 Wins!";
+}else if(num2>num1){
+  document.querySelector("h1").innerHTML="Player 2 Wins!";
+}
